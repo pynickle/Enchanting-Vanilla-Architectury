5f9d4ffe58f811f8c43fc205b03ef6c5f4f5cf3c
ENC_Euphony <2330458484@qq.com>
2025-07-23 15:17:21 +0800

    Fix

--- neoforge/src/main/java/com/euphony/enc_vanilla/neoforge/EncVanillaNeoforge.java
+++ neoforge/src/main/java/com/euphony/enc_vanilla/neoforge/EncVanillaNeoforge.java
@@ -1,6 +1,7 @@
 package com.euphony.enc_vanilla.neoforge;
 
 import com.euphony.enc_vanilla.EncVanilla;
+import net.minecraft.world.entity.npc.Villager;
 import net.neoforged.fml.common.Mod;
 
 @Mod(EncVanilla.MOD_ID)
